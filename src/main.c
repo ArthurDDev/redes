6de5667ca367e97abc7281840bcd1e2328f1d07d
ArthurDDev <ArthurDDev@gmail.com>
2026-04-29 10:11:14 -0300

    [add] Enviar e receber mensagens

--- src/main.c
+++ src/main.c
@@ -12,28 +12,18 @@ int main()
 
     char data[] = "eusouumamensagembemlonga";
     message m = {20, M_DATA, data};
-    void *buffer;
-    int tam = create_message(m, &buffer);
-    if (send(CON.socket, buffer, tam, 0) == -1){
-        printf("Deu merda\n");
-    }
-    unsigned char *x = buffer;
-    for (int i = 0; i < 24; i ++)
-        printf("%x ", x[i]);
-    printf("\n");
+    send_message(m);
 
 #else
 
     while (1) {
-        char buffer[25];
-        if (recv(CON.socket, &buffer, 24, 0) == -1)
-            printf("Erro\n");
-        else {
-            for (int i = 0; i < 24; i ++)
-                printf("%x ", buffer[i]);
-            printf("\n");
-            break;
-        }
+       message m = recieve_data();
+
+        printf("Mensagem:\nTamanho: %ld | Tipo: %d, Dados:\n", m.size, m.type);
+        unsigned char *data = m.data;
+        for (int i = 0; i < m.size; i ++)
+            printf("%x ", data[i]);
+        printf("\n");
     }
 
 #endif
--- src/message.c
+++ src/message.c
@@ -1,13 +1,12 @@
 #include "message.h"
+#include "net.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-size_t create_message(message m, void **dest)
+size_t create_frame(message m, void **dest)
 {
-    // Marcador de Início | Tamanho | Sequencia | Tipo | Dados | CRC
-    // 1 byte | 5 bits | 6 bits | 5 bits | n bytes | 1 byte
 
     size_t size = m.size + 4;
     char *str = malloc(size);
@@ -15,15 +14,57 @@ size_t create_message(message m, void **dest)
         fprintf(stderr, "Erro ao alocar memória\n");
         exit(1);
     }
-    char seq = 0;
+    char seq = next_seq();
 
     str[0] = 0b01111110;
     str[1] = m.size << 3 | seq >> 5;
     str[2] = seq << 3 | m.type;
-    str[size-1] = 0; // TODO CRC
+    str[size-1] = 0; // TODO: CRC
 
     memcpy(&str[3], m.data, m.size);
 
     *dest = str;
     return size;
+}
+
+void *delete_frame(void *dest)
+{
+    if (dest != NULL)
+        free(dest);
+    return NULL;
+}
+
+void delete_message(message *m)
+{
+    if (m == NULL)
+        return;
+
+    if (m->data != NULL)
+        free(m->data);
+
+    m->data = NULL;
+    m->size = 0;
+}
+
+message decode_message(void *src)
+{
+    if (src == NULL) {
+        fprintf(stderr, "Ponteiro de mensagem inválido\n");
+        exit(1);
+    }
+
+    char *frame = src;
+    message m;
+
+    m.size = frame[1] >> 3;
+    m.data = malloc(m.size);
+    if (m.data == NULL) {
+        fprintf(stderr, "Erro ao alocar memória\n");
+        exit(1);
+    }
+
+    memcpy(m.data, &frame[3], m.size);
+    m.type = frame[2] & 0b00011111;
+
+    return m;
 }
