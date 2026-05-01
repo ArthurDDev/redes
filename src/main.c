4a84d1a9060dfc1c1389ce37f11f6fa99e6277f2
leozbertoldi <leozbertoldi@gmail.com>
2026-05-01 12:20:19 -0300

    [fix] tamanho errado

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main()
 #ifdef SERVER
 
     char data[] = "eusouumamensagembemlonga";
-    message m = {20, M_DATA, data};
+    message m = {24, M_DATA, data};
     void *buffer;
     int tam = create_frame(m, &buffer);
 
@@ -30,12 +30,17 @@ int main()
     while (1) {
        message m = receive_data();
 
-        printf("Mensagem:\nTamanho: %ld | Tipo: %d, Dados:\n", m.size, m.type);
+        printf("Mensagem:\nTamanho: %ld | Tipo: %d | Dados:\n", m.size, m.type);
         unsigned char *data = m.data;
 
         // mensagem em hexa
         for (size_t i = 0; i < m.size; i ++)
             printf("%x ", data[i]);
+        printf("\n");
+
+        // mensagem normal
+        for (size_t i = 0; i < m.size; i ++)
+            printf("%c", data[i]);
         printf("\n");
     }
 
